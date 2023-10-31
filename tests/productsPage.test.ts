fd18d7d7259648cd4aea50b456eafedfeaca2ab5
silky09 <silkysantosh01@gmail.com>
2023-10-31 09:21:38 +0100

    delete file

--- tests/productsPage.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-
-test('ProductPage', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/inventory.html')
-    await expect(page).toHaveScreenshot()
-})
