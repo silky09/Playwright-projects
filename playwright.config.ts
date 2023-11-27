390e15ad93354f35607b023862b8eb714c7a6290
silky09 <silkysantosh01@gmail.com>
2023-11-27 14:22:56 +0100

    ✅ update config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html',
+  reporter: [["dot"], ["json", { outputFile: 'test-results.json' }]],
  
   
   use: {
